dd9e513cb1d2805099bff0da4bc2150760925b1b
zhaixubo <113840032+zhaixubo@users.noreply.github.com>
2023-07-27 19:31:03 +0800

    Initial Commit

---
+++ 自定义KVO/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  自定义KVO
+//
+//  Created by 翟旭博 on 2023/7/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ 自定义KVO/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  自定义KVO
+//
+//  Created by 翟旭博 on 2023/7/27.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
